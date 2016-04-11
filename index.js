c28bd9729a45941751722ac8246aa67e2debbf9d
Ryan <ryan@traveloka.com>
2016-04-11 17:17:06 +0700

    remove config dependencies

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-const config = require('config');
 const fetch = require('node-fetch');
 const NodeCache = require('node-cache');
 const qs = require('querystring');
@@ -7,8 +6,16 @@ const shipCache = new NodeCache({ stdTTL: 100, checkperiod: 120, errorOnMissing:
 const headers = {
   key: null
 };
+const requestData = {
+  APIHostName: 'http://api.rajaongkir.com',
+  APIUrl: {
+    city: '/starter/city',
+    province: '/starter/province',
+    cost: '/starter/cost'
+  }
+};
 exports.getAPIResult = (apiPath, method, headers, result) => {
-  const hostName = `${config.get('APIHostName')}${apiPath}`;
+  const hostName = `${requestData.APIHostName}${apiPath}`;
   const options = {
     method,
     headers
@@ -16,8 +23,8 @@ exports.getAPIResult = (apiPath, method, headers, result) => {
 
   let apiObject;
   switch (apiPath) {
-    case config.get('APIUrl.province'): apiObject = 'province'; break;
-    case config.get('APIUrl.city'): apiObject = 'city'; break;
+    case requestData.APIUrl.province: apiObject = 'province'; break;
+    case requestData.APIUrl.city: apiObject = 'city'; break;
     default: apiObject = '';
   }
 
@@ -40,24 +47,24 @@ exports.init = key => {
   headers.key = key;
 };
 exports.getAllProvince = result => {
-  this.getAPIResult(config.get('APIUrl.province'), 'GET', headers, result);
+  this.getAPIResult(requestData.APIUrl.province, 'GET', headers, result);
 };
 
 exports.getProvinceById = (provinceId, result) => {
   if (provinceId && Number.isInteger(provinceId)) {
-    this.getAPIResult(`${config.get('APIUrl.province')}?id=${provinceId}`, 'GET', result);
+    this.getAPIResult(`${requestData.APIUrl.province}?id=${provinceId}`, 'GET', result);
   } else {
     console.error('provinceId must be filled and integer');
   }
 };
 
 exports.getAllcity = result => {
-  this.getAPIResult(config.get('APIUrl.city'), 'GET', headers, result);
+  this.getAPIResult(requestData.APIUrl.city, 'GET', headers, result);
 };
 
 exports.getCityById = (cityId, result) => {
   if (cityId && Number.isInteger(cityId)) {
-    this.getAPIResult(`${config.get('APIUrl.city')}?id=${cityId}`, 'GET', headers, result);
+    this.getAPIResult(`${requestData.APIUrl.city}?id=${cityId}`, 'GET', headers, result);
   } else {
     console.error('cityId must be filled and integer');
   }
@@ -65,8 +72,8 @@ exports.getCityById = (cityId, result) => {
 
 exports.getShippingCost = (origin, destination, weight, courier, result) => {
   const costHeaders = Object.assign(headers, { 'content-type': 'application/x-www-form-urlencoded' });
-  const apiPath = config.get('APIUrl.cost');
-  const hostName = `${config.get('APIHostName')}${apiPath}`;
+  const apiPath = requestData.APIUrl.cost;
+  const hostName = `${requestData.APIHostName}${apiPath}`;
   const options = {
     method: 'POST',
     headers: costHeaders,
--- test.js
+++ test.js
@@ -2,14 +2,14 @@
 const index = require('./index');
 index.init('b06f682018ccadef06d7260481971b02');
 
-index.getAllProvince(result => {
-  console.log('done');
-});
+// index.getAllProvince(result => {
+//   console.log(result);
+// });
 
 // index.getCityById(39, result => {
 //   console.log(result);
 // });
 
-// index.getShippingCost('501', '114', 1000, 'jne', res => {
-//   console.log(JSON.stringify(res));
-// });
+index.getShippingCost('501', '114', 1000, 'jne', res => {
+  console.log(res);
+});
