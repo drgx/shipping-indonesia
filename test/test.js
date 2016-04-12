cb4f91bd19511d9c3630298b5c9c7b20838cbac4
Ryan <ryan@traveloka.com>
2016-04-12 18:11:27 +0700

    add chai and fix test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 const shipping = require('../index');
+const should = require('chai').should();
 shipping.init(process.env.KEY);
 describe('Shipping Indonesia', function() {
   it('check api response', function(done) {
     this.timeout(10000);
     shipping.getCityById(39, function (result) {
+      console.log(result);
+      should.exist(result);
       done();
     });
   });
