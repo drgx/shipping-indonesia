31dd22cd6e89d5a28d95884ddddd2eb56838142c
Ryan <ryan@traveloka.com>
2016-04-12 17:45:59 +0700

    fix test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 const shipping = require('../index');
-// b06f682018ccadef06d7260481971b02
 shipping.init(process.env.KEY);
 describe('Shipping Indonesia', function() {
   it('check api response', function(done) {
